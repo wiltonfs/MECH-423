5e7f41a5dbfdc1d89dd453f9a98ffbe8605c5dca
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-09-10 10:46:37 -0700

    Exercise 8 - All but gesture detection

---
+++ Lab 1/Exercises/Exercise8/PunchDetector.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Exercise4
+{
+    partial class PunchDetector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.dataTimer = new System.Windows.Forms.Timer(this.components);
+            this.aXDisplay = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.aYDisplay = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.aZDisplay = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.orientationLabel = new System.Windows.Forms.Label();
+            this.unitsCheckbox = new System.Windows.Forms.CheckBox();
+            this.visualTimer = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.avgZDisplay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.avgYDisplay = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.avgXDisplay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.avgLabel = new System.Windows.Forms.Label();
+            this.serialBufferSizeLabel = new System.Windows.Forms.Label();
+            this.queueSizeLabel = new System.Windows.Forms.Label();
+            this.gestureDisplayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(13, 13);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.serialComboBox.TabIndex = 0;
+            this.serialComboBox.SelectedIndexChanged += new System.EventHandler(this.serialComboBox_SelectedIndexChanged);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(141, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(129, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh Serial Ports";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(276, 10);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(119, 23);
+            this.disconnectButton.TabIndex = 2;
+            this.disconnectButton.Text = "Disconnect Serial";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // dataTimer
+            // 
+            this.dataTimer.Enabled = true;
+            this.dataTimer.Interval = 50;
+            this.dataTimer.Tick += new System.EventHandler(this.dataTimer_Tick);
+            // 
+            // aXDisplay
+            // 
+            this.aXDisplay.Location = new System.Drawing.Point(42, 94);
+            this.aXDisplay.Name = "aXDisplay";
+            this.aXDisplay.Size = new System.Drawing.Size(100, 20);
+            this.aXDisplay.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "aX:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // aYDisplay
+            // 
+            this.aYDisplay.Location = new System.Drawing.Point(201, 94);
+            this.aYDisplay.Name = "aYDisplay";
+            this.aYDisplay.Size = new System.Drawing.Size(100, 20);
+            this.aYDisplay.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(172, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "aY:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // aZDisplay
+            // 
+            this.aZDisplay.Location = new System.Drawing.Point(366, 94);
+            this.aZDisplay.Name = "aZDisplay";
+            this.aZDisplay.Size = new System.Drawing.Size(100, 20);
+            this.aZDisplay.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(337, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "aZ:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // orientationLabel
+            // 
+            this.orientationLabel.AutoSize = true;
+            this.orientationLabel.Location = new System.Drawing.Point(13, 124);
+            this.orientationLabel.Name = "orientationLabel";
+            this.orientationLabel.Size = new System.Drawing.Size(96, 13);
+            this.orientationLabel.TabIndex = 18;
+            this.orientationLabel.Text = "No board detected";
+            // 
+            // unitsCheckbox
+            // 
+            this.unitsCheckbox.AutoSize = true;
+            this.unitsCheckbox.Location = new System.Drawing.Point(16, 71);
+            this.unitsCheckbox.Name = "unitsCheckbox";
+            this.unitsCheckbox.Size = new System.Drawing.Size(109, 17);
+            this.unitsCheckbox.TabIndex = 19;
+            this.unitsCheckbox.Text = "Display in m/(ss)?";
+            this.unitsCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // visualTimer
+            // 
+            this.visualTimer.Enabled = true;
+            this.visualTimer.Interval = 250;
+            this.visualTimer.Tick += new System.EventHandler(this.visualTimer_Tick);
+            // 
+            // avgZDisplay
+            // 
+            this.avgZDisplay.Location = new System.Drawing.Point(366, 206);
+            this.avgZDisplay.Name = "avgZDisplay";
+            this.avgZDisplay.Size = new System.Drawing.Size(100, 20);
+            this.avgZDisplay.TabIndex = 26;
+            this.avgZDisplay.Text = "Not enough vals";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "avgZ:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // avgYDisplay
+            // 
+            this.avgYDisplay.Location = new System.Drawing.Point(201, 206);
+            this.avgYDisplay.Name = "avgYDisplay";
+            this.avgYDisplay.Size = new System.Drawing.Size(100, 20);
+            this.avgYDisplay.TabIndex = 24;
+            this.avgYDisplay.Text = "Not enough vals";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "avgY:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // avgXDisplay
+            // 
+            this.avgXDisplay.Location = new System.Drawing.Point(42, 206);
+            this.avgXDisplay.Name = "avgXDisplay";
+            this.avgXDisplay.Size = new System.Drawing.Size(100, 20);
+            this.avgXDisplay.TabIndex = 22;
+            this.avgXDisplay.Text = "Not enough vals";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "avgX:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // avgLabel
+            // 
+            this.avgLabel.AutoSize = true;
+            this.avgLabel.Location = new System.Drawing.Point(8, 191);
+            this.avgLabel.Name = "avgLabel";
+            this.avgLabel.Size = new System.Drawing.Size(215, 13);
+            this.avgLabel.TabIndex = 27;
+            this.avgLabel.Text = "Average acceleration over last n data points";
+            // 
+            // serialBufferSizeLabel
+            // 
+            this.serialBufferSizeLabel.AutoSize = true;
+            this.serialBufferSizeLabel.Location = new System.Drawing.Point(13, 267);
+            this.serialBufferSizeLabel.Name = "serialBufferSizeLabel";
+            this.serialBufferSizeLabel.Size = new System.Drawing.Size(112, 13);
+            this.serialBufferSizeLabel.TabIndex = 28;
+            this.serialBufferSizeLabel.Text = "Serial buffer size: NaN";
+            // 
+            // queueSizeLabel
+            // 
+            this.queueSizeLabel.AutoSize = true;
+            this.queueSizeLabel.Location = new System.Drawing.Point(13, 289);
+            this.queueSizeLabel.Name = "queueSizeLabel";
+            this.queueSizeLabel.Size = new System.Drawing.Size(88, 13);
+            this.queueSizeLabel.TabIndex = 29;
+            this.queueSizeLabel.Text = "Queue size: NaN";
+            // 
+            // gestureDisplayLabel
+            // 
+            this.gestureDisplayLabel.AutoSize = true;
+            this.gestureDisplayLabel.Location = new System.Drawing.Point(13, 393);
+            this.gestureDisplayLabel.Name = "gestureDisplayLabel";
+            this.gestureDisplayLabel.Size = new System.Drawing.Size(120, 13);
+            this.gestureDisplayLabel.TabIndex = 30;
+            this.gestureDisplayLabel.Text = "Simple Punch detected!";
+            // 
+            // PunchDetector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gestureDisplayLabel);
+            this.Controls.Add(this.queueSizeLabel);
+            this.Controls.Add(this.serialBufferSizeLabel);
+            this.Controls.Add(this.avgLabel);
+            this.Controls.Add(this.avgZDisplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.avgYDisplay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.avgXDisplay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.unitsCheckbox);
+            this.Controls.Add(this.orientationLabel);
+            this.Controls.Add(this.aZDisplay);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.aYDisplay);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.aXDisplay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.serialComboBox);
+            this.Name = "PunchDetector";
+            this.Text = "Punch Detector";
+            this.Load += new System.EventHandler(this.SerialDemo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialComboBox;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer dataTimer;
+        private System.Windows.Forms.TextBox aXDisplay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox aYDisplay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox aZDisplay;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label orientationLabel;
+        private System.Windows.Forms.CheckBox unitsCheckbox;
+        private System.Windows.Forms.Timer visualTimer;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox avgZDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox avgYDisplay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox avgXDisplay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label avgLabel;
+        private System.Windows.Forms.Label serialBufferSizeLabel;
+        private System.Windows.Forms.Label queueSizeLabel;
+        private System.Windows.Forms.Label gestureDisplayLabel;
+    }
+}
+
---
+++ Lab 1/Exercises/Exercise8/PunchDetector.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise4
+{
+    enum ExpectedNextRead
+    {
+        LEAD, X, Y, Z
+    }
+
+    public partial class PunchDetector : Form
+    {
+        // Editable parameters
+        int accelerationAveragingCount = 100;
+
+        // Concurrent Queue
+        ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
+
+        // Vec3 Queue
+        ExpectedNextRead expectedNextRead = ExpectedNextRead.LEAD;
+        Queue<Vec3> accelQueue = new Queue<Vec3>();
+        Vec3 mostRecentAccel = new Vec3();
+        Vec3 bias = new Vec3(-125f, -126f, -125f);
+        Vec3 scale = new Vec3(0.35f, 0.35f, 0.35f);
+
+        public PunchDetector()
+        {
+            InitializeComponent();
+            refreshComboBox();
+        }
+
+        private void SerialDemo_Load(object sender, EventArgs e)
+        {
+            avgLabel.Text = "Average acceleration over last " + accelerationAveragingCount + " data points:";
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshComboBox();
+        }
+
+        private void refreshComboBox()
+        {
+            // Refresh the combo box with the available COM ports
+            serialComboBox.Items.Clear();
+            serialComboBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (serialComboBox.Items.Count == 0)
+                serialComboBox.Text = "No COM ports!";
+            else
+                serialComboBox.SelectedIndex = 0;
+        }
+
+        private void serialComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = serialComboBox.Text;
+                serialPort1.Open();
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+        private void dataTimer_Tick(object sender, EventArgs e)
+        {
+            ReadQueue();
+        }
+
+        private void visualTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayInstantAccel();
+            DisplayOrientation();
+            AvgAndDisplayLastNPoints();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int newByte = 0;
+            int bytesToRead;
+            bytesToRead = serialPort1.BytesToRead;
+
+            // Display serial buffer size here
+            serialBufferSizeLabel.Text = "Serial buffer size: " + bytesToRead;
+            while (bytesToRead != 0)
+            {
+                newByte = serialPort1.ReadByte();
+                dataQueue.Enqueue(newByte);
+
+                bytesToRead = serialPort1.BytesToRead;
+            }
+        }
+
+        // --- Acceleration and Queue Related Functions ---
+
+        private void ReadQueue()
+        {
+            // Display queue size here
+            queueSizeLabel.Text = "Concurrent queue size: " + dataQueue.Count;
+
+            int nextVal;
+            bool succ = dataQueue.TryDequeue(out nextVal);
+            while (succ)
+            {
+                if (nextVal == 255)
+                {
+                    // Indicates new data frame
+                    expectedNextRead = ExpectedNextRead.X;
+                } 
+                else
+                {
+                    float correctedX = (nextVal + bias.X) * scale.X;
+                    float correctedY = (nextVal + bias.Y) * scale.Y;
+                    float correctedZ = (nextVal + bias.Z) * scale.Z;
+                    switch (expectedNextRead)
+                    {
+                        case ExpectedNextRead.LEAD:
+                            break;
+                        case ExpectedNextRead.X:
+                            accelQueue.Enqueue(new Vec3(correctedX, 0.0f, 0.0f));
+                            expectedNextRead++;
+                            break;
+                        case ExpectedNextRead.Y:
+                            accelQueue.Last<Vec3>().Y = correctedY;
+                            expectedNextRead++;
+                            break;
+                        case ExpectedNextRead.Z:
+                            accelQueue.Last<Vec3>().Z = correctedZ;
+                            mostRecentAccel = accelQueue.Last();
+                            expectedNextRead = ExpectedNextRead.LEAD;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                succ = dataQueue.TryDequeue(out nextVal);
+            }
+        }
+
+        private void DisplayOrientation()
+        {
+            if (accelQueue.Count > 0)
+                orientationLabel.Text = "The board's top face is the " + mostRecentAccel.UpAxisSign() + " " + mostRecentAccel.UpAxis() + " axis";
+        }
+
+        private void DisplayInstantAccel()
+        {
+            if (accelQueue.Count > 0)
+            {
+                if (unitsCheckbox.Checked)
+                {
+                    aXDisplay.Text = mostRecentAccel.X.ToString();
+                    aYDisplay.Text = mostRecentAccel.Y.ToString();
+                    aZDisplay.Text = mostRecentAccel.Z.ToString();
+                } 
+                else
+                {
+                    // De-convert bias and offset
+                    float correctedX = (mostRecentAccel.X / scale.X) - bias.X;
+                    float correctedY = (mostRecentAccel.Y / scale.Y) - bias.Y;
+                    float correctedZ = (mostRecentAccel.Z / scale.Z) - bias.Z;
+                    aXDisplay.Text = correctedX.ToString();
+                    aYDisplay.Text = correctedY.ToString();
+                    aZDisplay.Text = correctedZ.ToString();
+                }
+            }
+        }
+
+        private void AvgAndDisplayLastNPoints()
+        {
+            if (accelQueue.Count >= accelerationAveragingCount)
+            {
+                // Have enough to avg at least
+
+                // Skip to get just the last accelerationAveragingCount
+                int skip = accelQueue.Count - accelerationAveragingCount;
+                float avgX = 0.0f, avgY = 0.0f, avgZ = 0.0f;
+
+                // THIS IS NOT PERFORMANT. ANY LAG SHOULD BE INVESTIGATED HERE
+                int i = 0;
+                foreach (Vec3 accelVector in accelQueue)
+                {
+                    if (i  >= skip)
+                    {
+                        avgX += accelVector.X;
+                        avgY += accelVector.Y;
+                        avgZ += accelVector.Z;
+                    }
+                    i++;
+                }
+
+                // Process the averages
+                avgX = avgX / accelerationAveragingCount;
+                avgY = avgY / accelerationAveragingCount;
+                avgZ = avgZ / accelerationAveragingCount;
+                if (!unitsCheckbox.Checked)
+                {
+                    // De-convert bias and offset
+                    float correctedX = (mostRecentAccel.X / scale.X) - bias.X;
+                    float correctedY = (mostRecentAccel.Y / scale.Y) - bias.Y;
+                    float correctedZ = (mostRecentAccel.Z / scale.Z) - bias.Z;
+                }
+                // Reduce to 3 decimal points
+                avgX = (float)Math.Round((double)avgX, 3);
+                avgY = (float)Math.Round((double)avgY, 3);
+                avgZ = (float)Math.Round((double)avgZ, 3);
+
+                // Display average acceleration
+                avgXDisplay.Text = avgX.ToString();
+                avgYDisplay.Text = avgY.ToString();
+                avgZDisplay.Text = avgZ.ToString();
+            }
+        }
+    }
+}
