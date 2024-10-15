489cecd730c1d720b4b7cee1b9125a9d6e8249f4
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-10-15 14:50:27 -0700

    Lab 2 - Ex 4 & 5 using FSWLib

--- Lab 2/Exercise 4/main.c
+++ Lab 2/Exercise 4/main.c
@@ -16,104 +16,66 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	// --------------------------
-	// ------- UART SETUP -------
-	// --------------------------
-
-	// Setup Subsystem Master Clock (SMCLK) to 1Mhz
-    CSCTL0 = CSKEY;         // Set the clock password.  (L) pg. 80
-    CSCTL1 |= DCOFSEL_3;    // Set DCO Freq to 8 MHz.   (L) pg. 81
-    CSCTL2 |= SELM__DCOCLK; // Set MCLK to run on DCO.  (L) pg. 82
-    CSCTL2 |= SELS__DCOCLK; // Set SMCLK to run on DCO. (L) pg. 82
-    CSCTL3 |= DIVS__8;      // SMCLK divider of 8       (L) pg. 83
-
-	// Set the clock source and configure UART
-    UCA0CTLW0 |= UCSWRST;        // Put eUSCI in reset  (L) pg. 495
-    UCA0CTLW0 |= UCSSEL__SMCLK;  // SMCLK as source     (L) pg. 495
-    UCA0CTLW0 &= ~(BIT9 | BITA); // UART mode           (L) pg. 495
-    // Put target baud and BRCLK into equations         (L) pg. 487
-    // Or check handy table for common settings         (L) pg. 490
-    // For 1MHz clock and 9600 baud:
-    // UCOS16 = 1
-    // UCBRx = 6
-    // UCBRFx = 8
-    // UCBRSx = 0x20
-    UCA0BRW = 6;                // Prescaler control    (L) pg. 516
-    UCA0MCTLW = 0x2081;         // Assemble settings    (L) pg. 497
-    // Set the other settings. 8 data bits, no parity, 1 stop bit. These are default but set anyway.
-    UCA0CTLW0 &= ~UC7BIT;        // 8 data bits         (L) pg. 495
-    UCA0CTLW0 &= ~UCPEN;         // Disable Parity      (L) pg. 495
-    UCA0CTLW0 &= ~UCSPB;         // 1 Stop bit          (L) pg. 495
-
-    // Configure pins P2.0 and P2.1 for UART as Receive and Transmit
-    // P2.0 TXD, P2.1 RXD
-    P2SEL0 &= ~(BIT0 | BIT1);       // (M) pg. 74
-    P2SEL1 |=  (BIT0 | BIT1);       // (M) pg. 74
-
-    // Start UART
-    UCA0CTLW0 &= ~UCSWRST;       // Undo reset on eUSCI (L) pg. 495
-
-    // --------------------------
-    // ----- END UART SETUP -----
-    // --------------------------
-
-    // LECTURE EXAMPLE CODE
-    UCA0IE |= UCRXIE; // Enable RX interrupt
-    _EINT(); // Global interrupt enable
-
-    // Configure P3.6 (LED7) and P3.7 (LED8) as outputs
-    // Using these for debug
-    //      (M) page 81 and 82
-    P3DIR |=   (BIT6 | BIT7);
-    P3SEL1 &= ~(BIT6 | BIT7);
-    P3SEL0 &= ~(BIT6 | BIT7);
-
-    // Configure PJ.0 (LED1) as digital output
-    //      (M) page 86
-    PJDIR |=   (BIT0);
-    PJSEL1 &= ~(BIT0);
-    PJSEL0 &= ~(BIT0);
-
-    // Turn off LED8 for debug visual
-    P3OUT &= ~BIT7;
-
-    // Turn on LED7 for debug visual
-    P3OUT |= BIT6;
+	StandardClockSetup_8Mhz_1Mhz();
+	SetupLEDPins(ALL_LEDs);
+	TurnOffLED(ALL_LEDs);
+	StandardUART0Setup_9600_8();
+	UCA0IE |= UCRXIE;           // Enable RX interrupt
+	__enable_interrupt();       // Enable global interrupts
 
     // Inside the while loop, send 'a' periodically
     while (1) {
-        // UCA0AFG is the register with the interrupt flags
-        // UCTXIFG is the bit of the flag of interest
-        // Hold until the flag is up
-        //while (!(UCA0IFG & UCTXIFG));
+        DelayMillis_8Mhz(500);
+        UART_TX_Char_BLOCKING('a');
 
-        //UCA0TXBUF = 'a'; // Transmit the letter 'a'
-        //__delay_cycles(600000);        // Delay before next transmission
+        // Heart beat to display general program progression
+        // If this stops, you are stuck in an interrupt
+        ToggleLED(LED2);
     }
 
 	return 0;
 }
 
 #pragma vector = USCI_A0_VECTOR
-__interrupt void uart_echo_ISR(void)
-{
-    // Turn on LED8 for debug visual
-    P3OUT |= BIT7;
-
-    unsigned char RxByte = 0;
-    RxByte = UCA0RXBUF; // Read from the receive buffer
-    while (!(UCA0IFG & UCTXIFG)); // Hold until the "finish last transmission" flag is up
-    UCA0TXBUF = RxByte; // Write to transmit buffer
-    while (!(UCA0IFG & UCTXIFG)); // Hold until the "finish last transmission" flag is up
-    UCA0TXBUF = RxByte + 1; // Write to transmit buffer
-
-    if (RxByte == 'j')
+__interrupt void uart_ISR(void) {
+    if (UCA0IV == USCI_UART_UCRXIFG)    // Receive buffer full. (L) pg. 504
     {
-        // Turn on LED1
-        PJOUT |= BIT0;
-    } else if (RxByte == 'k')
+        // -------------------------------------------
+        // --- Handle RX interrupt (data received) ---
+        // -------------------------------------------
+        // Toggle LED8 for debug visual
+        ToggleLED(LED8);
+
+        volatile unsigned char RxByte = UCA0RXBUF; // Read from the receive buffer
+
+        UART_TX_Char_BLOCKING(RxByte);
+        UART_TX_Char_BLOCKING(RxByte + 1);
+
+        if (RxByte == 'j')
+        {
+            // Turn on LED1
+            TurnOnLED(LED1);
+        } else if (RxByte == 'k')
+        {
+            // Turn off LED1
+            TurnOffLED(LED1);
+        }
+
+        return;
+
+    }
+    else if (UCA0IV == USCI_UART_UCTXIFG || UCA0IV == USCI_UART_UCTXCPTIFG)     // Transmit buffer empty OR Transmit complete. (L) pg. 504
     {
-        // Turn off LED1
-        PJOUT &= ~BIT0;
+        // ---------------------------------------------------
+        // --- Handle TX interrupt (transmission complete) ---
+        // ---------------------------------------------------
+        // Turn on LED7 for debug visual
+        TurnOnLED(LED7);
+
+        return;
+    }
+    else if (UCA0IV == USCI_UART_UCSTTIFG)  // Start bit received. (L) pg. 504
+    {
+        return;
     }
 }
--- Lab 2/Exercise 5/main.c
+++ Lab 2/Exercise 5/main.c
@@ -1,5 +1,11 @@
 #include <msp430.h> 
 #include "../FSWLib/FSWLib.c"
+// Exercise 5
+// Felix Wilton
+// Oct 3 2024
+// (L) = MSP430 Family User Guide [576 pages]
+// (M) = MSPE430 Datasheet [124 pages]
+// (S) = MSP-EXP430FR5739 User Guide [28 pages]
 
 
 /**
@@ -10,35 +16,12 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	StandardClockSetup_8Mhz_1Mhz();
-	SetupLEDPins(ALL_LEDs);
+    SetupLEDPins(ALL_LEDs);
+    TurnOffLED(ALL_LEDs);
 
-	// Setup Timer B in the "up count" mode
-	TB1CTL |= TBCLR;            // Clear Timer B            (L) pg.372
-	TB1CTL |= (BIT4);           // Up mode                  (L) pg. 372
-	TB1CTL |= TBSSEL__SMCLK;    // Clock source select      (L) pg. 372
-    TB1CTL |= (BIT7 | BIT6);    // 1/8 divider (125 kHz)    (L) pg. 372
-    TB1CCR0  = 249;             // now 500 Hz               (L) pg. 377
-    // 249 = 496 Hz
-    // 247 = 500 Hz
-    // 245 = 504 Hz
-
-	// Configure TB1.1 to produce a 500 Hz square wave
-    TB1CCTL1 = OUTMOD_7;        // Reset/Set mode           (L) pg. 365, 366, and 375
-    TB1CCR1 = 125;              // 50% duty cycle
-
-    // Configure TB1.2 to produce a 500 Hz square wave at 25% duty cycle
-    TB1CCTL2 = OUTMOD_7;        // Reset/Set mode           (L) pg. 365, 366, and 375
-    TB1CCR2 = 62;               // 25% duty cycle
-
-	// Output TB1.1 on P3.4
-    P3DIR  |=  BIT4;            // Set P3.4 as output       (M) pg. 81
-    P3SEL1 &= ~BIT4;            // Select TB1.1 function    (M) pg. 81
-    P3SEL0 |=  BIT4;            // Select TB1.1 function    (M) pg. 81
-
-	// Output TB1.2 on P3.5
-    P3DIR  |=  BIT5;            // Set P3.5 as output       (M) pg. 81
-    P3SEL1 &= ~BIT5;            // Select TB1.2 function    (M) pg. 81
-    P3SEL0 |=  BIT5;            // Select TB1.2 function    (M) pg. 81
+    TimerB1Setup_UpCount_125kHz(249);   // now 500 Hz               (L) pg. 377
+    TimerB1_PWM(1, 125);
+    TimerB1_PWM(2, 62);
 
 	while (1)
 	{
