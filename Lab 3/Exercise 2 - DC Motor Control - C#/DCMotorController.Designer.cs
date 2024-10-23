68fc6afbd0688aa492fb2bb207914272711f7901
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-10-22 18:57:53 -0700

    Exercise 2 - Rough draft of C# interface

---
+++ Lab 3/Exercise 2 - DC Motor Control - C#/DCMotorController.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DCMotorController
+{
+    partial class DCMotorController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.dataTimer = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.orientationLabel = new System.Windows.Forms.Label();
+            this.visualTimer = new System.Windows.Forms.Timer(this.components);
+            this.outQueueDisplay = new System.Windows.Forms.Label();
+            this.postStringVisual = new System.Windows.Forms.Label();
+            this.preStringVisual = new System.Windows.Forms.Label();
+            this.DC1_SpeedInput = new System.Windows.Forms.TrackBar();
+            this.DC1_SendButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.DC1_SpeedInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(13, 13);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.serialComboBox.TabIndex = 0;
+            this.serialComboBox.SelectedIndexChanged += new System.EventHandler(this.serialComboBox_SelectedIndexChanged);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(141, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(129, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh Serial Ports";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(276, 10);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(119, 23);
+            this.disconnectButton.TabIndex = 2;
+            this.disconnectButton.Text = "Disconnect Serial";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // dataTimer
+            // 
+            this.dataTimer.Enabled = true;
+            this.dataTimer.Interval = 50;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "DC Motor Speed:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // orientationLabel
+            // 
+            this.orientationLabel.AutoSize = true;
+            this.orientationLabel.Location = new System.Drawing.Point(14, 37);
+            this.orientationLabel.Name = "orientationLabel";
+            this.orientationLabel.Size = new System.Drawing.Size(96, 13);
+            this.orientationLabel.TabIndex = 18;
+            this.orientationLabel.Text = "No board detected";
+            // 
+            // visualTimer
+            // 
+            this.visualTimer.Enabled = true;
+            this.visualTimer.Tick += new System.EventHandler(this.visualTimer_Tick);
+            // 
+            // outQueueDisplay
+            // 
+            this.outQueueDisplay.AutoSize = true;
+            this.outQueueDisplay.Location = new System.Drawing.Point(13, 211);
+            this.outQueueDisplay.Name = "outQueueDisplay";
+            this.outQueueDisplay.Size = new System.Drawing.Size(146, 13);
+            this.outQueueDisplay.TabIndex = 23;
+            this.outQueueDisplay.Text = "Out Queue: 255 255 255 255";
+            this.outQueueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // postStringVisual
+            // 
+            this.postStringVisual.AutoSize = true;
+            this.postStringVisual.Location = new System.Drawing.Point(14, 380);
+            this.postStringVisual.Name = "postStringVisual";
+            this.postStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.postStringVisual.TabIndex = 33;
+            // 
+            // preStringVisual
+            // 
+            this.preStringVisual.AutoSize = true;
+            this.preStringVisual.Location = new System.Drawing.Point(14, 367);
+            this.preStringVisual.Name = "preStringVisual";
+            this.preStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.preStringVisual.TabIndex = 34;
+            // 
+            // DC1_SpeedInput
+            // 
+            this.DC1_SpeedInput.Location = new System.Drawing.Point(17, 114);
+            this.DC1_SpeedInput.Maximum = 5;
+            this.DC1_SpeedInput.Minimum = -5;
+            this.DC1_SpeedInput.Name = "DC1_SpeedInput";
+            this.DC1_SpeedInput.Size = new System.Drawing.Size(394, 45);
+            this.DC1_SpeedInput.TabIndex = 35;
+            // 
+            // DC1_SendButton
+            // 
+            this.DC1_SendButton.Location = new System.Drawing.Point(418, 114);
+            this.DC1_SendButton.Name = "DC1_SendButton";
+            this.DC1_SendButton.Size = new System.Drawing.Size(115, 23);
+            this.DC1_SendButton.TabIndex = 36;
+            this.DC1_SendButton.Text = "Send Command";
+            this.DC1_SendButton.UseVisualStyleBackColor = true;
+            this.DC1_SendButton.Click += new System.EventHandler(this.DC1_SendButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Max CCW";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Max CW";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txTimer
+            // 
+            this.txTimer.Enabled = true;
+            this.txTimer.Interval = 15;
+            this.txTimer.Tick += new System.EventHandler(this.txTimer_Tick);
+            // 
+            // DCMotorController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DC1_SendButton);
+            this.Controls.Add(this.DC1_SpeedInput);
+            this.Controls.Add(this.preStringVisual);
+            this.Controls.Add(this.postStringVisual);
+            this.Controls.Add(this.outQueueDisplay);
+            this.Controls.Add(this.orientationLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.serialComboBox);
+            this.Name = "DCMotorController";
+            this.Text = "DC Motor Controller";
+            this.Load += new System.EventHandler(this.DcMotorController_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DC1_SpeedInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialComboBox;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer dataTimer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label orientationLabel;
+        private System.Windows.Forms.Timer visualTimer;
+        private System.Windows.Forms.Label outQueueDisplay;
+        private System.Windows.Forms.Label postStringVisual;
+        private System.Windows.Forms.Label preStringVisual;
+        private System.Windows.Forms.TrackBar DC1_SpeedInput;
+        private System.Windows.Forms.Button DC1_SendButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer txTimer;
+    }
+}
+
---
+++ Lab 3/Exercise 2 - DC Motor Control - C#/DCMotorController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCMotorController
+{
+    enum COMM_BYTE
+    {
+        STOP_DC_1, CW_DC_1, CCW_DC_1
+    }
+
+    public partial class DCMotorController : Form
+    {
+        Queue<byte> outgoingQueue = new Queue<byte>();
+        Queue<int> incomingQueue = new Queue<int>();
+
+        public DCMotorController()
+        {
+            InitializeComponent();
+            refreshComboBox();
+        }
+
+        void refreshComboBox()
+        {
+            // Refresh the combo box with the available COM ports
+            serialComboBox.Items.Clear();
+            serialComboBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (serialComboBox.Items.Count == 0)
+                serialComboBox.Text = "No COM ports!";
+            else
+                serialComboBox.SelectedIndex = 0;
+        }
+
+        private void DcMotorController_Load(object sender, EventArgs e)
+        {}
+
+        private void serialComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = serialComboBox.Text;
+                serialPort1.Open();
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int newByte = 0;
+            int bytesToRead;
+            bytesToRead = serialPort1.BytesToRead;
+            while (bytesToRead != 0)
+            {
+                newByte = serialPort1.ReadByte();
+                incomingQueue.Enqueue(newByte);
+
+                bytesToRead = serialPort1.BytesToRead;
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshComboBox();
+        }
+        private void RefreshVisuals()
+        {
+            string queue = "Out queue: ";
+            foreach (byte b in outgoingQueue)
+            {
+                queue += b + " ";
+            }
+            outQueueDisplay.Text = queue;
+
+        }
+
+        private void SendDCMotorCommand(int speed)
+        {
+            COMM_BYTE COM = COMM_BYTE.STOP_DC_1;
+            if (speed > 0)
+                COM = COMM_BYTE.CW_DC_1;
+            if (speed < 0)
+                COM = COMM_BYTE.CCW_DC_1;
+
+            byte D1 = MostSignificant(SpeedToPWM(speed));
+            byte D2 = LeastSignificant(SpeedToPWM(speed));
+
+            QueueOutgoing(COM, D1, D2);
+        }
+
+        private void QueueOutgoing(COMM_BYTE COMMenum, byte D1, byte D2)
+        {
+            byte COMM = CommandByteToByte(COMMenum);
+
+            byte ESC = 0;
+            
+            if (COMM > 254)
+            {
+                COMM = 254; ESC += 1;
+            }
+            if (D1 > 254)
+            {
+                D1 = 254; ESC += 2;
+            }
+            if (D2 > 254)
+            {
+                D2 = 254; ESC += 4;
+            }
+
+            outgoingQueue.Enqueue(255);
+            outgoingQueue.Enqueue(COMM);
+            outgoingQueue.Enqueue(D1);
+            outgoingQueue.Enqueue(D2);
+            outgoingQueue.Enqueue(ESC);
+        }
+
+        private byte CommandByteToByte(COMM_BYTE COMenum)
+        {
+            switch (COMenum)
+            {
+                case COMM_BYTE.STOP_DC_1: return 0;
+                case COMM_BYTE.CW_DC_1: return 1;
+                case COMM_BYTE.CCW_DC_1: return 2;
+                default: return 0;
+            }
+        }
+
+        private uint SpeedToPWM(int speed)
+        {
+            if (speed == 0)
+                return 0;
+            speed = Math.Abs(speed);
+
+            uint offset = 5;
+            uint bias = 1;
+            uint PWM = ((uint)speed*bias + offset);
+            return PWM;
+        }
+
+        private byte MostSignificant(uint value)
+        {
+            return (byte)((value >> 8) & 0xFF);
+        }
+
+        private byte LeastSignificant(uint value)
+        {
+            return (byte)(value & 0xFF);
+        }
+
+        public static int Clamp(int value, int min, int max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        private void DC1_SendButton_Click(object sender, EventArgs e)
+        {
+            SendDCMotorCommand(DC1_SpeedInput.Value);
+        }
+
+        private void visualTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshVisuals();
+        }
+
+        private void txTimer_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen && serialPort1.BytesToWrite == 0)
+            {
+                byte tx = outgoingQueue.Dequeue();
+                serialPort1.Write(new byte[] { tx }, 0, 1);
+            }
+        }
+    }
+}
