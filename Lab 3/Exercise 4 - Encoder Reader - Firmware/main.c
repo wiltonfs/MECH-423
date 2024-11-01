4d026a894a31b59e51d8a05f3855086907cbf975
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-11-01 14:43:52 -0700

    L3 Ex4 - C - MVP

--- Lab 3/Exercise 4 - Encoder Reader - Firmware/main.c
+++ Lab 3/Exercise 4 - Encoder Reader - Firmware/main.c
@@ -1,20 +1,32 @@
 #include <msp430.h> 
 #include "../MotorLib/General.c"
 #include "../MotorLib/Com.c"
+#include "../MotorLib/DC.c"
+#include "../MotorLib/Encoder.c"
 
 // Lab 3 - Exercise 4
 // Felix Wilton & Lazar Stanojevic
 // Oct 31 2024
 
+// ~~~~~ This Firmware Uses ~~~~~
+// DC Motor:
+//      P3.6 - CW control
+//      P3.7 - CCW control
+//      P2.1 - PWM speed control
+//      Timer B2
+// Encoder:
+//      P1.1 - CW step pulse
+//      P1.2 - CCW step pulse
+//      Timer A0 - CW counter
+//      Timer A1 - CCW counter
+//      Timer B1 - High accuracy TX timer
+// Debug:
+//      P1.6 - Heartbeat LED
+//      P1.7 - Velocity interrupt LED
+#define HEARTBEAT_LED BIT6
+#define VEL_IE_LED BIT7
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//unsigned int CWsteps = 0;
-//unsigned int CCWsteps = 0;
-#define CW_STEPS_SOURCE TA1R
-#define CCW_STEPS_SOURCE TA0R
-
-#define CW_SPIN 0
-#define CCW_SPIN 1
-#define BRAKE 2
 
 MessagePacket txPacket = EMPTY_MESSAGE_PACKET;
 PACKET_FRAGMENT nextTx = START_BYTE;
@@ -22,118 +34,39 @@ PACKET_FRAGMENT nextTx = START_BYTE;
 MessagePacket IncomingPacket = EMPTY_MESSAGE_PACKET;
 volatile PACKET_FRAGMENT NextRead = START_BYTE;
 
-void EncoderTimerSetup(void)
-{
-    //initialize P1.1 & P1.2 &  to primary capture mode (TA0 & TA1)
-    P1SEL0 |= (BIT1 + BIT2);
-    P1SEL1 &= ~(BIT1 + BIT2);
-    P1DIR &= ~(BIT1 + BIT2);
-
-    // Setup Timer A0 in the "continous count" mode
-    TA0CTL |= TACLR;            // Clear Timer A                (L) pg. 349
-    TA0CTL |= MC_2;             // Continous mode               (L) pg. 349
-    TA0CTL |= TASSEL__TACLK;    // Clock source select          (L) pg. 349
-
-    // Setup Timer A1 in the "continous count" mode
-    TA1CTL |= TACLR;            // Clear Timer A                (L) pg. 349
-    TA1CTL |= MC_2;             // Continous mode               (L) pg. 349
-    TA1CTL |= TASSEL__TACLK;    // Clock source select          (L) pg. 349
-}
-
-void ClearEncoderCounts(void)
-{
-    TA1CTL |= TACLR;            // Clear Timer A1                (L) pg. 349
-    TA0CTL |= TACLR;            // Clear Timer A0                (L) pg. 349
-}
-
-void DCMotor_Setup(){
-
-    //initialize pins 2.1 to primary mode (TB2.1)
-    P2SEL0 |= BIT1;
-    P2SEL1 &= ~BIT1;
-    P2DIR |= BIT1;
-
-    // Setup Timer B in the "up count" mode
-    TB2CTL |= TBCLR;             // Clear Timer B            (L) pg.372
-    TB2CTL |= (BIT4);            // Up mode                  (L) pg. 372
-    TB2CTL |= TBSSEL__ACLK;      // Clock source select      (L) pg. 372
-    TB2CTL &= ~(BIT7 | BIT6);    // No divider               (L) pg. 372
-    TB2CCR0 = 65535;     // What we count to         (L) pg. 377
-
-    //Set up pins P3.6 and P3.7 to output rotation direction
-    P3DIR |= (BIT6 | BIT7);
-    P3SEL1 &= ~(BIT6 | BIT7);
-    P3SEL0 &= ~(BIT6 | BIT7);
-
-    //Turn both to low
-    P3OUT &= ~(BIT6 | BIT7);
-}
-
-void DCMotor_PWM(unsigned int dutyCycleCounter){
-
-    TB2CCTL1 = OUTMOD_7;        // Reset/Set mode           (L) pg. 365, 366, and 375
-    TB2CCR1 = dutyCycleCounter;
-}
-
-void DCMotor_Command(char CMD){
-
-    if(CMD == CW_SPIN) {
-        // CW spin command to motor driver
-        P3OUT |= BIT6;
-        P3OUT &= ~BIT7;
-    } else if(CMD == CCW_SPIN){
-        // CCW spin command to motor driver
-        P3OUT &= ~BIT6;
-        P3OUT |= BIT7;
-    } else if(CMD == BRAKE){
-        // Stop spin command to motor driver
-        P3OUT &= ~(BIT6 + BIT7);
-    }
-}
-
 void ProcessCompletePacket() {
-
-    // Modify the square wave duty cycle
-    DCMotor_PWM(IncomingPacket.combined);
-
-    // Handle the command byte
-    if (IncomingPacket.comm == DCM_CW) {
-
-        //DCMotor_Command(CW_SPIN);
-
+    if (IncomingPacket.comm == DEBUG_ECHO_REQUEST) {
+        COM_UART1_MakeAndTransmitMessagePacket_BLOCKING(DEBUG_ECHO_RESPONSE, IncomingPacket.d1, IncomingPacket.d2);
+        return;
+    } else if (IncomingPacket.comm == DCM_CW) {
+        DC_Spin(IncomingPacket.combined, CLOCKWISE);
+        return;
     } else if (IncomingPacket.comm == DCM_CCW) {
-
-        //DCMotor_Command(CCW_SPIN);
-
+        DC_Spin(IncomingPacket.combined, COUNTERCLOCKWISE);
+        return;
     } else if (IncomingPacket.comm == DCM_BRAKE) {
-
-        //DCMotor_Command(BRAKE);
+        DC_Brake();
+        return;
     }
+
+
+    // Unhandled COMM byte, notify of that
+    COM_UART1_MakeAndTransmitMessagePacket_BLOCKING(DEBUG_UNHANDLED_COMM, IncomingPacket.comm, 0);
 }
 
-void EncoderVelocityTimerSetup(){
-
-    //Uses to TB1.1 to trigger every ~40ms to compile data packet, then every ~200ms a full packet is sent.
-
-    //initialize pins 3.4 to primary mode (TB1.1)
-    P3SEL0 |= BIT4;
-    P3SEL1 &= ~BIT4;
-    P3DIR |= BIT4;
+void EncoderVelocityTimerSetup()
+{
+    // Set up a timer (TB1) to interrupt every 40 ms (25 Hz)
+    // The interrupt sends a packet fragment
+    // So a full packet is sent every 40*5=200 ms (5 Hz)
 
     // Setup Timer B in the "up count" mode
-    TB1CTL |= TBCLR;             // Clear Timer B            (L) pg.372
-    TB1CTL |= (BIT4);            // Up mode                  (L) pg. 372
-    TB1CTL |= TBSSEL__SMCLK;      // Clock source select     (L) pg. 372
-    TB1CTL |= (BIT7 | BIT6);    // 1/8 divider (125 kHz)     (L) pg. 372
-    TB1CCR0 = 5000;     // What we count to         (L) pg. 377
-
-    //upCountTarget = 5000 -> T = 40.0 ms
-
-    //Enable timer interrupt
-    TB1CCTL1 |= CCIE;
-
-    TB1CCTL1 = OUTMOD_7;        // Reset/Set mode           (L) pg. 365, 366, and 375
-    TB1CCR1 = 2500;
+    TB1CTL |= TBCLR;            // Clear Timer B            (L) pg.372
+    TB1CTL |= (BIT4);           // Up mode                  (L) pg. 372
+    TB1CTL |= TBSSEL__SMCLK;    // Clock source select      (L) pg. 372
+    TB1CTL |= (BIT7 | BIT6);    // 1/8 divider (125 kHz)    (L) pg. 372
+    //upCountTarget = 5000, then 125000/5000= 25 Hz
+    TB1CCR0 = 5000;             // What we count to         (L) pg. 377
 }
 
 
@@ -141,56 +74,33 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     StandardClockSetup_8Mhz_1Mhz();
-
     StandardUART1Setup_9600_8();
     UCA1IE |= UCRXIE;           // Enable RX interrupt
 
-    EncoderTimerSetup();
-
     //DC Motor set-up
-    DCMotor_Setup();
-    DCMotor_PWM(32000);         // 50% duty cycle, P2.1 output
-    DCMotor_Command(CW_SPIN);
+    DC_SetupDCMotor();
+    DC_Spin(32000, CLOCKWISE);  // 50% duty cycle, P2.1 output
 
-    //Set-up timer for transmitting at 40ms (200ms for full packet)
+    //Encoder set-up
+    ENCODER_SetupEncoder();
     EncoderVelocityTimerSetup();
+    TB1CCTL0 |= CCIE;           // Enable interrupt   (L) pg. 375
 
-    // Set up P1.6 as LED output
-    P1DIR |= BIT6;
-    P1SEL1 &= ~BIT6;
-    P1SEL0 &= ~BIT6;
-
+    // Set up Debug LEDs    (M) pg. 73
+    P1DIR  |=   (HEARTBEAT_LED | VEL_IE_LED);
+    P1SEL1 &=~  (HEARTBEAT_LED | VEL_IE_LED);
+    P1SEL0 &=~  (HEARTBEAT_LED | VEL_IE_LED);
     // Turn the LEDs off
-    P1OUT &= ~BIT6;
+    P1OUT  &=~  (HEARTBEAT_LED | VEL_IE_LED);
 
-    //__enable_interrupt();        // Enable global interrupts
-    _EINT();
+    __enable_interrupt();        // Enable global interrupts
 
     while(1)
     {
         DelayMillis_8Mhz(200);
 
-        P1OUT ^= BIT6;
-
-//        if (COM_UART1_TransmitMessagePacketFragment(&txPacket, &nextTx))
-//        {
-//            // Build the next packet
-//            if (CW_STEPS_SOURCE > CCW_STEPS_SOURCE)
-//            {
-//                txPacket.comm = ENC_ROT_DELTA_CW;
-//                txPacket.combined = CW_STEPS_SOURCE - CCW_STEPS_SOURCE;
-//            } else {
-//                txPacket.comm = ENC_ROT_DELTA_CCW;
-//                txPacket.combined = CCW_STEPS_SOURCE - CW_STEPS_SOURCE;
-//            }
-//
-//            // Clear the steps counter
-//            ClearEncoderCounts();
-//
-//            COM_SeperateDataBytes(&txPacket);
-//        }
-//        CWsteps += 1;
-//        CCWsteps += 55;
+        // Heartbeat
+        P1OUT ^= HEARTBEAT_LED;
     }
 
     return 0;
@@ -198,8 +108,8 @@ int main(void)
 
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void transmission_timer_ISR(void){
-    //transmit the data packet
-    UART_TX_Char_BLOCKING(244);
+    // Toggle ISR visual
+    P1OUT ^= VEL_IE_LED;
 }
 
 
@@ -215,14 +125,5 @@ __interrupt void uart_ISR(void)
             // returns True if the packet is now complete
             ProcessCompletePacket();
         }
-        return;
-    }
-    else if (UCA1IV == USCI_UART_UCTXIFG || UCA1IV == USCI_UART_UCTXCPTIFG)     // Transmit buffer empty OR Transmit complete. (L) pg. 504
-    {
-        return;
-    }
-    else if (UCA1IV == USCI_UART_UCSTTIFG)  // Start bit received. (L) pg. 504
-    {
-        return;
     }
 }
--- Lab 3/MotorLib/Encoder.c
+++ Lab 3/MotorLib/Encoder.c
@@ -17,7 +17,8 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
+#define CW_STEPS_SOURCE TA1R
+#define CCW_STEPS_SOURCE TA0R
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -25,13 +26,68 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+void ENCODER_SetupEncoder();
+// Sets up the pins and timers for the encoder.
+// P1.1 - CW pulses, P1.2 - CCW pulses.
+// Uses Timer A0 and Timer A1
 
+void ENCODER_ClearEncoderCounts();
+// Clears the encoder counts, stored in TA0R and TA1R
+
+unsigned int ENCODER_GetNetSteps_CW();
+// Returns the net number of steps in the clockwise direction, or zero if the net direction is counterclockwise
+
+unsigned int ENCODER_GetNetSteps_CCW();
+// Returns the net number of steps in the counterclockwise direction, or zero if the net direction is clockwise
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~ Function Definitions ~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void ENCODER_SetupEncoder()
+{
+    // Set P1.1 & P1.2 as the clock signal for TA1 & TA0
+    // Set them to TAxCLK mode, (M) pg. 70
+    P1DIR  &=~ (BIT1 + BIT2);
+    P1SEL1 |=  (BIT1 + BIT2);
+    P1SEL0 &=~ (BIT1 + BIT2);
+
+    // Setup Timer A0 in the "continuous count" mode
+    TA0CTL |= TACLR;            // Clear Timer A0               (L) pg. 349
+    TA0CTL |= MC__CONTINUOUS;   // Continuous mode              (L) pg. 349
+    TA0CTL |= TASSEL__TACLK;    // Clock source: TAxCLK         (L) pg. 349
+
+    // Setup Timer A1 in the "continuous count" mode
+    TA1CTL |= TACLR;            // Clear Timer A1               (L) pg. 349
+    TA1CTL |= MC__CONTINUOUS;   // Continous mode               (L) pg. 349
+    TA1CTL |= TASSEL__TACLK;    // Clock source: TAxCLK         (L) pg. 349
+
+    ENCODER_ClearEncoderCounts();
+}
+
+void ENCODER_ClearEncoderCounts()
+{
+    TA0CTL |= TACLR;            // Clear Timer A0       (L) pg. 349
+    TA1CTL |= TACLR;            // Clear Timer A1       (L) pg. 349
+}
+
+unsigned int ENCODER_GetNetSteps_CW()
+{
+    if (CW_STEPS_SOURCE > CCW_STEPS_SOURCE)
+        return CW_STEPS_SOURCE - CCW_STEPS_SOURCE;
+    else
+        return 0;
+}
+
+unsigned int ENCODER_GetNetSteps_CCW()
+{
+    if (CCW_STEPS_SOURCE > CW_STEPS_SOURCE)
+        return CCW_STEPS_SOURCE - CW_STEPS_SOURCE;
+    else
+        return 0;
+}
 
 
 
