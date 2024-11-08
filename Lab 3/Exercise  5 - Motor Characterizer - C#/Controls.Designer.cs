f84a989b5afef8857a1856280e74be7aa4875ab3
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-11-08 14:52:56 -0800

    L3 Ex5 - C# - Motor characterizing

---
+++ Lab 3/Exercise  5 - Motor Characterizer - C#/Controls.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace DCMotorController
+{
+    partial class Controls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.boardConnectedLabel = new System.Windows.Forms.Label();
+            this.visualTimer = new System.Windows.Forms.Timer(this.components);
+            this.outQueueDisplay = new System.Windows.Forms.Label();
+            this.postStringVisual = new System.Windows.Forms.Label();
+            this.preStringVisual = new System.Windows.Forms.Label();
+            this.txTimer = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.txHistoryDisplay = new System.Windows.Forms.TextBox();
+            this.rxHistoryDisplay = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rxTimer = new System.Windows.Forms.Timer(this.components);
+            this.ClearMeasurementButton = new System.Windows.Forms.Button();
+            this.StepInputButton = new System.Windows.Forms.Button();
+            this.StepInputTextbox = new System.Windows.Forms.TextBox();
+            this.totalRXdDisplay = new System.Windows.Forms.Label();
+            this.PositionalCommandTextbox = new System.Windows.Forms.TextBox();
+            this.PositionalCommandButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MeasurementDisplayBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(13, 13);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.serialComboBox.TabIndex = 0;
+            this.serialComboBox.SelectedIndexChanged += new System.EventHandler(this.serialComboBox_SelectedIndexChanged);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(141, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(129, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh Serial Ports";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(276, 10);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(119, 23);
+            this.disconnectButton.TabIndex = 2;
+            this.disconnectButton.Text = "Disconnect Serial";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // boardConnectedLabel
+            // 
+            this.boardConnectedLabel.AutoSize = true;
+            this.boardConnectedLabel.Location = new System.Drawing.Point(14, 37);
+            this.boardConnectedLabel.Name = "boardConnectedLabel";
+            this.boardConnectedLabel.Size = new System.Drawing.Size(96, 13);
+            this.boardConnectedLabel.TabIndex = 18;
+            this.boardConnectedLabel.Text = "No board detected";
+            // 
+            // visualTimer
+            // 
+            this.visualTimer.Enabled = true;
+            this.visualTimer.Tick += new System.EventHandler(this.visualTimer_Tick);
+            // 
+            // outQueueDisplay
+            // 
+            this.outQueueDisplay.AutoSize = true;
+            this.outQueueDisplay.Location = new System.Drawing.Point(13, 346);
+            this.outQueueDisplay.Name = "outQueueDisplay";
+            this.outQueueDisplay.Size = new System.Drawing.Size(109, 13);
+            this.outQueueDisplay.TabIndex = 23;
+            this.outQueueDisplay.Text = "Bytes pending TX: 12";
+            this.outQueueDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // postStringVisual
+            // 
+            this.postStringVisual.AutoSize = true;
+            this.postStringVisual.Location = new System.Drawing.Point(14, 380);
+            this.postStringVisual.Name = "postStringVisual";
+            this.postStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.postStringVisual.TabIndex = 33;
+            // 
+            // preStringVisual
+            // 
+            this.preStringVisual.AutoSize = true;
+            this.preStringVisual.Location = new System.Drawing.Point(14, 367);
+            this.preStringVisual.Name = "preStringVisual";
+            this.preStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.preStringVisual.TabIndex = 34;
+            // 
+            // txTimer
+            // 
+            this.txTimer.Enabled = true;
+            this.txTimer.Interval = 15;
+            this.txTimer.Tick += new System.EventHandler(this.txTimer_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 367);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "Tx History:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txHistoryDisplay
+            // 
+            this.txHistoryDisplay.Location = new System.Drawing.Point(16, 384);
+            this.txHistoryDisplay.Multiline = true;
+            this.txHistoryDisplay.Name = "txHistoryDisplay";
+            this.txHistoryDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txHistoryDisplay.Size = new System.Drawing.Size(379, 54);
+            this.txHistoryDisplay.TabIndex = 48;
+            // 
+            // rxHistoryDisplay
+            // 
+            this.rxHistoryDisplay.Location = new System.Drawing.Point(409, 384);
+            this.rxHistoryDisplay.Multiline = true;
+            this.rxHistoryDisplay.Name = "rxHistoryDisplay";
+            this.rxHistoryDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.rxHistoryDisplay.Size = new System.Drawing.Size(379, 54);
+            this.rxHistoryDisplay.TabIndex = 49;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(406, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Rx History:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rxTimer
+            // 
+            this.rxTimer.Enabled = true;
+            this.rxTimer.Interval = 5;
+            this.rxTimer.Tick += new System.EventHandler(this.rxTimer_Tick);
+            // 
+            // ClearMeasurementButton
+            // 
+            this.ClearMeasurementButton.Location = new System.Drawing.Point(12, 65);
+            this.ClearMeasurementButton.Name = "ClearMeasurementButton";
+            this.ClearMeasurementButton.Size = new System.Drawing.Size(129, 23);
+            this.ClearMeasurementButton.TabIndex = 54;
+            this.ClearMeasurementButton.Text = "Clear Measurement";
+            this.ClearMeasurementButton.UseVisualStyleBackColor = true;
+            this.ClearMeasurementButton.Click += new System.EventHandler(this.StopMotorButton_Click);
+            // 
+            // StepInputButton
+            // 
+            this.StepInputButton.Location = new System.Drawing.Point(12, 94);
+            this.StepInputButton.Name = "StepInputButton";
+            this.StepInputButton.Size = new System.Drawing.Size(153, 23);
+            this.StepInputButton.TabIndex = 55;
+            this.StepInputButton.Text = "Step Input [PWM]";
+            this.StepInputButton.UseVisualStyleBackColor = true;
+            this.StepInputButton.Click += new System.EventHandler(this.StepInputButton_Click);
+            // 
+            // StepInputTextbox
+            // 
+            this.StepInputTextbox.Location = new System.Drawing.Point(171, 96);
+            this.StepInputTextbox.Name = "StepInputTextbox";
+            this.StepInputTextbox.Size = new System.Drawing.Size(100, 20);
+            this.StepInputTextbox.TabIndex = 56;
+            // 
+            // totalRXdDisplay
+            // 
+            this.totalRXdDisplay.AutoSize = true;
+            this.totalRXdDisplay.Location = new System.Drawing.Point(14, 333);
+            this.totalRXdDisplay.Name = "totalRXdDisplay";
+            this.totalRXdDisplay.Size = new System.Drawing.Size(107, 13);
+            this.totalRXdDisplay.TabIndex = 57;
+            this.totalRXdDisplay.Text = "Total bytes RXd: 100";
+            this.totalRXdDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // PositionalCommandTextbox
+            // 
+            this.PositionalCommandTextbox.Location = new System.Drawing.Point(171, 125);
+            this.PositionalCommandTextbox.Name = "PositionalCommandTextbox";
+            this.PositionalCommandTextbox.Size = new System.Drawing.Size(100, 20);
+            this.PositionalCommandTextbox.TabIndex = 59;
+            // 
+            // PositionalCommandButton
+            // 
+            this.PositionalCommandButton.Location = new System.Drawing.Point(12, 123);
+            this.PositionalCommandButton.Name = "PositionalCommandButton";
+            this.PositionalCommandButton.Size = new System.Drawing.Size(153, 23);
+            this.PositionalCommandButton.TabIndex = 58;
+            this.PositionalCommandButton.Text = "Positional Command [counts]";
+            this.PositionalCommandButton.UseVisualStyleBackColor = true;
+            this.PositionalCommandButton.Click += new System.EventHandler(this.PositionalCommandButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 60;
+            this.label1.Text = "Measurement:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MeasurementDisplayBox
+            // 
+            this.MeasurementDisplayBox.Location = new System.Drawing.Point(362, 86);
+            this.MeasurementDisplayBox.Multiline = true;
+            this.MeasurementDisplayBox.Name = "MeasurementDisplayBox";
+            this.MeasurementDisplayBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MeasurementDisplayBox.Size = new System.Drawing.Size(379, 133);
+            this.MeasurementDisplayBox.TabIndex = 61;
+            // 
+            // Controls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MeasurementDisplayBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PositionalCommandTextbox);
+            this.Controls.Add(this.PositionalCommandButton);
+            this.Controls.Add(this.totalRXdDisplay);
+            this.Controls.Add(this.StepInputTextbox);
+            this.Controls.Add(this.StepInputButton);
+            this.Controls.Add(this.ClearMeasurementButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rxHistoryDisplay);
+            this.Controls.Add(this.txHistoryDisplay);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.preStringVisual);
+            this.Controls.Add(this.postStringVisual);
+            this.Controls.Add(this.outQueueDisplay);
+            this.Controls.Add(this.boardConnectedLabel);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.serialComboBox);
+            this.Name = "Controls";
+            this.Text = "System Characterization";
+            this.Load += new System.EventHandler(this.DcMotorController_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialComboBox;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label boardConnectedLabel;
+        private System.Windows.Forms.Timer visualTimer;
+        private System.Windows.Forms.Label outQueueDisplay;
+        private System.Windows.Forms.Label postStringVisual;
+        private System.Windows.Forms.Label preStringVisual;
+        private System.Windows.Forms.Timer txTimer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txHistoryDisplay;
+        private System.Windows.Forms.TextBox rxHistoryDisplay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer rxTimer;
+        private System.Windows.Forms.Button ClearMeasurementButton;
+        private System.Windows.Forms.Button StepInputButton;
+        private System.Windows.Forms.TextBox StepInputTextbox;
+        private System.Windows.Forms.Label totalRXdDisplay;
+        private System.Windows.Forms.TextBox PositionalCommandTextbox;
+        private System.Windows.Forms.Button PositionalCommandButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MeasurementDisplayBox;
+    }
+}
+
---
+++ Lab 3/Exercise  5 - Motor Characterizer - C#/Controls.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DCMotorController
+{
+    enum PACKET_FRAGMENT
+    {
+        START_BYTE,
+        COM_BYTE,
+        D1_BYTE,
+        D2_BYTE,
+        ESCP_BYTE
+    }
+    struct MessagePacket
+    {
+        public byte comm;
+        public byte d1;
+        public byte d2;
+        public byte esc;
+        public ushort combined;
+    }
+    enum COMM_BYTE
+    {
+        DEBUG_ECHO_REQUEST = 0, DEBUG_ECHO_RESPONSE = 1, DEBUG_UNHANDLED_COMM = 7,
+
+        DCM_CW = 8, DCM_CCW = 9, DCM_BRAKE = 10,
+
+        STP_SINGLE_CW = 16, STP_SINGLE_CCW = 17, STP_CONT_CW = 18, STP_CONT_CCW = 19, STP_STOP = 20,
+
+        ENC_ROT_DELTA_CW = 24, ENC_ROT_DELTA_CCW = 25,
+
+        MES_RESET = 48, MES_REQ_STEP = 49, MES_REQ_POSITION = 50, MES_RESPONSE = 51
+    }
+
+    public partial class Controls : Form
+    {
+        // --------------------------
+        // ----- Members & Data -----
+        // --------------------------
+
+        // Input and output Serial queues
+        int totalBytesRXd = 0;
+        ConcurrentQueue<byte> outgoingQueue = new ConcurrentQueue<byte>();
+        ConcurrentQueue<int> incomingQueue = new ConcurrentQueue<int>();
+        PACKET_FRAGMENT expectedNextRx = PACKET_FRAGMENT.START_BYTE;
+        MessagePacket mostRecentPacket = new MessagePacket();
+
+        // ---------------------------------
+        // ----- Constructors and Load -----
+        // ---------------------------------
+        public Controls()
+        {
+            InitializeComponent();
+            refreshSerialConnectionsComboBox();
+        }
+
+        private void DcMotorController_Load(object sender, EventArgs e)
+        {}
+
+        // ---------------------
+        // ----- UI Events -----
+        // ---------------------
+
+        // ----- Connecting to board -----
+        private void serialComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = serialComboBox.Text;
+                serialPort1.Open();
+            }
+        }
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshSerialConnectionsComboBox();
+        }
+        private void StopMotorButton_Click(object sender, EventArgs e)
+        {
+            MeasurementDisplayBox.Text = "";
+            QueueOutgoing(COMM_BYTE.MES_RESET, 0, 0);
+        }
+        private void StepInputButton_Click(object sender, EventArgs e)
+        {
+            ushort speed = 0;
+            if (ushort.TryParse(StepInputTextbox.Text, out speed))
+            {
+                ushort PWM = (ushort)(speed * 655);
+                QueueOutgoing(COMM_BYTE.MES_REQ_STEP, PWM);
+            } else {
+                MessageBox.Show("Invalid step input, must be 0-100. Please enter a valid integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void PositionalCommandButton_Click(object sender, EventArgs e)
+        {
+            ushort position = 0;
+            if (ushort.TryParse(PositionalCommandTextbox.Text, out position))
+            {
+                QueueOutgoing(COMM_BYTE.MES_REQ_POSITION, position);
+            }
+            else
+            {
+                MessageBox.Show("Invalid location input, must be 0-65535. Please enter a valid integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // ----- Timers -----
+        private void visualTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshVisuals();
+        }
+        private void txTimer_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen && serialPort1.BytesToWrite == 0 && outgoingQueue.Count() > 0)
+            {
+                byte tx = 0;
+                bool succ = outgoingQueue.TryDequeue(out tx);
+                if (succ)
+                    serialPort1.Write(new byte[] { tx }, 0, 1);
+            }
+        }
+
+        private void rxTimer_Tick(object sender, EventArgs e)
+        {
+            if (!incomingQueue.IsEmpty)
+            {
+                int result = 0;
+                while(incomingQueue.TryDequeue(out result))
+                {
+                    RxStateMachine((byte)result);
+                }
+            }
+        }
+
+        // -------------------------
+        // ----- Serial Events -----
+        // -------------------------
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int newByte = 0;
+            int bytesToRead;
+            bytesToRead = serialPort1.BytesToRead;
+            while (bytesToRead != 0)
+            {
+                newByte = serialPort1.ReadByte();
+                incomingQueue.Enqueue(newByte);
+                totalBytesRXd++;
+
+                bytesToRead = serialPort1.BytesToRead;
+            }
+        }
+
+        // ------------------------
+        // ----- UI Functions -----
+        // ------------------------
+
+        private void RefreshVisuals()
+        {
+            outQueueDisplay.Text = $"Bytes pending TX: {outgoingQueue.Count}";
+            totalRXdDisplay.Text = $"Total bytes RXd: {totalBytesRXd}";
+
+            if (!serialPort1.IsOpen)
+            { boardConnectedLabel.Text = "No board detected"; }
+            else
+            { boardConnectedLabel.Text = "Board connected"; }
+        }
+        void refreshSerialConnectionsComboBox()
+        {
+            // Refresh the combo box with the available COM ports
+            serialComboBox.Items.Clear();
+            serialComboBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (serialComboBox.Items.Count == 0)
+                serialComboBox.Text = "No COM ports!";
+            else
+                serialComboBox.SelectedIndex = 0;
+        }
+
+        // -------------------------------------------------
+        // ----- Serial Functions & Messaging Protocol -----
+        // -------------------------------------------------
+
+        private void QueueOutgoing(COMM_BYTE COMMenum, ushort data)
+        {
+            QueueOutgoing(COMMenum, MostSignificant(data), LeastSignificant(data));
+        }
+        private void QueueOutgoing(COMM_BYTE COMMenum, byte D1, byte D2)
+        {
+            ushort combined = DataBytesToUShort(D1, D2);
+
+            byte COMM = (byte)COMMenum;
+
+            byte ESC = 0;
+
+            if (COMM > 254)
+            {
+                COMM = 254; ESC += 1;
+            }
+            if (D1 > 254)
+            {
+                D1 = 254; ESC += 2;
+            }
+            if (D2 > 254)
+            {
+                D2 = 254; ESC += 4;
+            }
+
+            outgoingQueue.Enqueue(255);
+            outgoingQueue.Enqueue(COMM);
+            outgoingQueue.Enqueue(D1);
+            outgoingQueue.Enqueue(D2);
+            outgoingQueue.Enqueue(ESC);
+
+            string packet = $"[{COMMenum}, {combined}] = [255 {COMM} {D1} {D2} {ESC}]\r\n";
+            txHistoryDisplay.Text += packet;
+        }
+
+        private void RxStateMachine(byte nextReceive)
+        {
+            if (nextReceive == 255)
+            {
+                expectedNextRx = PACKET_FRAGMENT.COM_BYTE;
+                return;
+            }
+
+            if (expectedNextRx == PACKET_FRAGMENT.COM_BYTE) {
+                mostRecentPacket.comm = nextReceive;
+            } else if (expectedNextRx == PACKET_FRAGMENT.D1_BYTE) {
+                mostRecentPacket.d1 = nextReceive;
+            } else if (expectedNextRx == PACKET_FRAGMENT.D2_BYTE) {
+                mostRecentPacket.d2 = nextReceive;
+            } else if (expectedNextRx == PACKET_FRAGMENT.ESCP_BYTE) {
+                mostRecentPacket.esc = nextReceive;
+                // Finished transmission
+                rxHistoryDisplay.Text += $"[255 {mostRecentPacket.comm} {mostRecentPacket.d1} {mostRecentPacket.d2} {mostRecentPacket.esc}] = ";
+                FormatCompletePacket(ref mostRecentPacket);
+                rxHistoryDisplay.Text += $"[{(COMM_BYTE)mostRecentPacket.comm}, {mostRecentPacket.combined}]\t\t";
+                UseCompletePacket(mostRecentPacket);
+            }
+
+            // Increment expected next read
+            if (expectedNextRx == PACKET_FRAGMENT.ESCP_BYTE)
+            {
+                expectedNextRx = PACKET_FRAGMENT.START_BYTE;
+            } else { expectedNextRx++; }
+            
+            
+        }
+
+        void FormatCompletePacket(ref MessagePacket MP)
+        {
+            // Handle the escape byte
+            if ((MP.esc & 0x1) > 0)
+                MP.comm = 255;
+            if ((MP.esc & 0x2) > 0)
+                MP.d1 = 255;
+            if ((MP.esc & 0x4) > 0)
+                MP.d2 = 255;
+
+            // Combine data 1 and 2
+            MP.combined = DataBytesToUShort(MP.d1, MP.d2);
+        }
+
+        void UseCompletePacket(MessagePacket MP)
+        {
+            // Use the command byte to use the rx
+            COMM_BYTE comm = (COMM_BYTE)MP.comm;
+
+            if (comm == COMM_BYTE.MES_RESPONSE) {
+                MeasurementDisplayBox.Text += $" {MP.combined},";
+            }
+        }
+
+        private byte MostSignificant(ushort value)
+        {
+            return (byte)((value >> 8) & 0xFF);
+        }
+
+        private byte LeastSignificant(ushort value)
+        {
+            return (byte)(value & 0xFF);
+        }
+
+        private ushort DataBytesToUShort(byte D1, byte D2)
+        {
+            return (ushort)(((ushort)D1 << 8) | (ushort)D2);
+        }
+    }
+}
