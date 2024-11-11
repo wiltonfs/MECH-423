bee6ceee5579613a27a86dea2d7ed2169d559a42
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-11-10 23:04:54 -0800

    L3 Ex5 - MATLAB - Started Simulink

--- Lab 3/Exercise 5 - Controls - Matlab/MotorCharacterization.m
+++ Lab 3/Exercise 5 - Controls - Matlab/MotorCharacterization.m
@@ -127,23 +127,25 @@ saveas(gcf, 'StepInput_200ms.png');
 close all; clear; clc;
 timeStep_ms = 1;
 DutyCycles = [25, 50, 75, 100];
+DutyCycleColors = {"#A2142F", "#7E2F8E", "#77AC30", "#0072BD"};
 
 data = readtable("StepInput_1ms.csv", 'NumHeaderLines', 1);
 time_ms = 0:timeStep_ms:timeStep_ms*(size(data(:,1))-1);
 figure('Position', [100, 100, 1200, 400]);
 sgtitle('Motor Response To Step Inputs - 1ms Sampling Period');
 for i = 1:length(DutyCycles)
+    color = DutyCycleColors{i};
     % Plotting position vs. time on the right subplot
     subplot(1, 2, 2);
     hold on;
     position_counts = data{:, i};
-    plot(time_ms/1000, position_counts);
+    plot(time_ms/1000, position_counts, 'Color', color, 'LineWidth', 2);
 
     % Plotting velocity vs. time on the left subplot
     subplot(1, 2, 1);
     hold on;
     [velocity_counts_ms, time_vel_ms] = compute_velocity_2(position_counts, time_ms, 15);
-    plot(time_vel_ms/1000, velocity_counts_ms*1000);
+    plot(time_vel_ms/1000, velocity_counts_ms*1000, 'Color', color, 'LineWidth', 2);
 end
 % Plotting position vs. time on the right subplot
 subplot(1, 2, 2);
@@ -169,17 +171,18 @@ time_ms = 0:timeStep_ms:timeStep_ms*(size(data(:,1))-1);
 figure('Position', [100, 100, 1200, 400]);
 sgtitle('Motor Response To Step Inputs - 5ms Sampling Period');
 for i = 1:length(DutyCycles)
+    color = DutyCycleColors{i};
     % Plotting position vs. time on the right subplot
     subplot(1, 2, 2);
     hold on;
     position_counts = data{:, i};
-    plot(time_ms/1000, position_counts);
+    plot(time_ms/1000, position_counts, 'Color', color, 'LineWidth', 2);
 
     % Plotting velocity vs. time on the left subplot
     subplot(1, 2, 1);
     hold on;
     [velocity_counts_ms, time_vel_ms] = compute_velocity_2(position_counts, time_ms, 15);
-    plot(time_vel_ms/1000, velocity_counts_ms*1000);
+    plot(time_vel_ms/1000, velocity_counts_ms*1000, 'Color', color, 'LineWidth', 2);
 end
 % Plotting position vs. time on the right subplot
 subplot(1, 2, 2);
@@ -197,8 +200,221 @@ legend('25% Duty Cycle', '50% Duty Cycle', '75% Duty Cycle', '100% Duty Cycle', 
 
 saveas(gcf, 'StepInput_5ms.png');
 
+%% Motor Characterization - Rise time, K, and Tau
+clear;close;clc;
 
-%% Motor Positional Response to Positional Command
+% Approximates velocity from time data
+function [riseTime, tau, K] = characterize(input, output, time)
+    % Calculate the rise time
+    minThreshold = 0.1*max(output);
+    maxThreshold = 0.9*max(output);
+    s = find(output > minThreshold, 1);
+    e = find(output > maxThreshold, 1);
+    riseTime = time(e)-time(s);
+
+    % Calculate time constant
+    timeConstantThreshold = (1-exp(-1)) * max(output);
+    i = find(output > timeConstantThreshold, 1);
+    tau = time(i);
+
+    % Calculate K
+    K = max(output)/max(input);
+end
+
+
+
+VToVelocity_counts_ms = @(V) (3.8620*V/5 -0.0474)*245/1000;
+
+Voltage = 100;
+timeStep_ms = 5;
+
+data = readtable("StepInput_5ms.csv", 'NumHeaderLines', 1);
+
+position_counts = data{:, 4};
+time_ms = 0:timeStep_ms:timeStep_ms*(size(data(:,1))-1);
+[velocity_counts_ms, time_vel_ms] = compute_velocity_2(position_counts, time_ms, 15);
+
+
+% Add some lead to the data
+L = 25;
+time_vel_ms = [timeStep_ms*(0:L-1), time_vel_ms + L*timeStep_ms];
+input_V = 5*(Voltage/100)*[zeros(1, L), ones(1, length(velocity_counts_ms))];
+expected_counts_ms = VToVelocity_counts_ms(input_V);
+velocity_counts_ms = [zeros(1, L), velocity_counts_ms];
+
+figure;
+hold on;
+
+yyaxis left
+plot(time_vel_ms/1000, velocity_counts_ms*1000, '-b');
+ylabel('Velocity [counts/s]');
+ylim([0, max(velocity_counts_ms*1000)*1.1]);
+
+yyaxis right
+plot(time_vel_ms/1000, input_V, '--r')
+ylabel('Voltage [V]')
+ylim([0, max(input_V)*1.1]);
+
+xlabel('Time [s]')
+title('Step Response Analysis')
+
+saveas(gcf, 'StepInput_Analysis.png');
+
+
+
+
+DutyCycles = [25, 50, 75, 100];
+
+timeStep_ms = 5;
+data = readtable("StepInput_5ms.csv", 'NumHeaderLines', 1);
+time_ms = 0:timeStep_ms:timeStep_ms*(size(data(:,1))-1);
+for i = 1:length(DutyCycles)
+    Voltage = DutyCycles(i);
+    position_counts = data{:, i};
+    [velocity_counts_ms, time_vel_ms] = compute_velocity_2(position_counts, time_ms, 15);
+    [riseTime, tau, K_ms] = characterize((Voltage*5/100), velocity_counts_ms, time_vel_ms);
+    K_s = K_ms * 1000;
+    fprintf('For the input signal of %d%%, rise time is %d ms, tau is %d ms, and K is %d.\n', Voltage, riseTime, tau, K_s);
+end
+
+%% Validate Transfer Function
+clear;close;clc;
+Km = 182.7; % counts/(seconds*volts)
+tau = 88.75*10^(-3); % seconds
+Motor_tf_velocity = tf(Km, [tau 1]);
+% I cheat here. Taking the average velocity in the way I did introduced
+% some lag, so when I characterized the system using that data, the tau was wrong. So I
+% guess and check on a new tau to make sure the position matches up too.
+tau = 50*10^(-3); % seconds
+Motor_tf_position = tf(Km, [tau 1 0]);
+
+DutyCycleColors = {"#A2142F", "#7E2F8E", "#77AC30", "#0072BD"};
+DutyCycles = [25, 50, 75, 100];
+
+timeStep_ms = 5;
+data = readtable("StepInput_5ms.csv", 'NumHeaderLines', 1);
+time_ms = 0:timeStep_ms:timeStep_ms*(size(data(:,1))-1);
+expected_time_s = 0:timeStep_ms/1000:max(time_ms)/1000;
+figure('Position', [100, 100, 1200, 400]);
+sgtitle('Motor Response To Step Inputs - 5ms Sampling Period');
+for i = 1:length(DutyCycles)
+    Voltage = 5*DutyCycles(i)/100;
+    color = DutyCycleColors{i};
+    % Get the expected values
+    expected_counts_s = Voltage * step(Motor_tf_velocity, expected_time_s);
+    expected_counts = Voltage * step(Motor_tf_position, expected_time_s);
+
+    % Plotting position vs. time on the right subplot
+    subplot(1, 2, 2);
+    hold on;
+    position_counts = data{:, i};
+    plot(expected_time_s, expected_counts, '--', 'Color', color, 'LineWidth', 2);
+    plot(time_ms/1000, position_counts, 'Color', color, 'LineWidth', 2);
+
+    % Plotting velocity vs. time on the left subplot
+    subplot(1, 2, 1);
+    hold on;
+    [velocity_counts_ms, time_vel_ms] = compute_velocity_2(position_counts, time_ms, 15);
+    plot(expected_time_s, expected_counts_s, '--', 'Color', color, 'LineWidth', 2);
+    plot(time_vel_ms/1000, velocity_counts_ms*1000, 'Color', color, 'LineWidth', 2);
+end
+% Plotting position vs. time on the right subplot
+subplot(1, 2, 2);
+title('System Positional Response');
+xlabel('Time [s]');
+ylabel('Position [counts]');
+%legend('25% Duty Cycle - Predicted','25% Duty Cycle - Measured', '50% Duty Cycle - Predicted', '50% Duty Cycle - Measured', '75% Duty Cycle - Predicted', '75% Duty Cycle - Measured', '100% Duty Cycle - Predicted', '100% Duty Cycle - Measured', 'location', 'northwest');
+
+% Plotting velocity vs. time on the left subplot
+subplot(1, 2, 1);
+title('Approximate System Velocity Response');
+xlabel('Time [s]');
+ylabel('Velocity [counts/s]');
+%legend('25% Duty Cycle - Predicted','25% Duty Cycle - Measured', '50% Duty Cycle - Predicted', '50% Duty Cycle - Measured', '75% Duty Cycle - Predicted', '75% Duty Cycle - Measured', '100% Duty Cycle - Predicted', '100% Duty Cycle - Measured', 'location', 'northeast');
+
+saveas(gcf, 'StepInput_5ms_expected.png');
+
+%% Motor Positional Response to Positional Command - NO SATURATION
+clc;clear;close all;
+Colors = {"#A2142F", "#7E2F8E", "#77AC30", "#0072BD"};
+
+command = 500; % counts
+V = 5; % volts
+Km = 182.7; % counts/(seconds*volts)
+tau = 50*10^(-3); % seconds
+Motor_tf_position = tf(Km, [tau 1 0]);
+Kps = [500, 1000, 5000];
+
+% Recorded data
+samplingTime_s = 1 / 250;
+sampleCount = 200;
+positionTo500_Kp500_counts = [ 0, 0, 1, 2, 4, 6, 8, 10, 12, 15, 18, 22, 25, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 73, 76, 80, 84, 89, 93, 98, 102, 107, 110, 115, 120, 124, 129, 133, 137, 142, 145, 150, 155, 159, 164, 168, 172, 177, 182, 186, 191, 195, 199, 204, 209, 213, 218, 223, 227, 231, 236, 241, 245, 250, 254, 258, 263, 268, 272, 277, 282, 286, 290, 295, 300, 304, 309, 314, 318, 322, 327, 332, 336, 341, 345, 350, 354, 359, 364, 368, 373, 377, 382, 385, 390, 394, 399, 401, 406, 410, 414, 418, 422, 426, 429, 432, 435, 439, 442, 445, 448, 450, 452, 455, 457, 459, 462, 463, 465, 467, 469, 470, 471, 473, 474, 475, 476, 478, 479, 480, 481, 482, 483, 483, 484, 485, 486, 487, 487, 488, 489, 489, 490, 490, 491, 492, 492, 493, 493, 494, 494, 495, 495, 496, 497, 497, 498, 498, 499, 499, 500, 500, 500, 501, 501, 501, 501, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502];
+positionTo500_Kp5000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 14, 18, 21, 24, 27, 31, 35, 39, 43, 47, 50, 54, 58, 63, 67, 71, 76, 79, 84, 88, 92, 97, 101, 105, 110, 114, 119, 123, 128, 133, 136, 141, 145, 150, 155, 159, 163, 168, 172, 177, 182, 186, 191, 195, 200, 204, 209, 214, 218, 223, 227, 231, 236, 241, 245, 250, 254, 259, 263, 268, 273, 276, 281, 285, 290, 294, 299, 304, 308, 312, 317, 322, 326, 331, 336, 340, 344, 349, 354, 358, 363, 368, 372, 376, 381, 386, 390, 395, 400, 404, 408, 413, 418, 423, 427, 431, 436, 440, 445, 450, 455, 459, 463, 468, 473, 477, 482, 487, 491, 495, 500, 504, 508, 510, 513, 514, 514, 513, 512, 510, 509, 507, 505, 503, 501, 499, 497, 496, 494, 494, 493, 493, 493, 494, 494, 495, 496, 498, 499, 500, 502, 503, 504, 505, 505, 506, 506, 506, 505, 504, 504, 503, 502, 501, 500, 499, 498, 498, 497, 496, 496, 496, 496, 496, 497, 497, 498, 498, 499, 499, 499, 500, 500, 500, 500, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501];
+positionTo500_Kp1000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 15, 18, 21, 25, 28, 31, 35, 39, 43, 47, 50, 54, 58, 63, 67, 71, 76, 79, 84, 88, 93, 98, 102, 106, 110, 115, 120, 124, 129, 133, 137, 142, 146, 151, 156, 160, 164, 169, 174, 178, 183, 188, 192, 196, 201, 205, 209, 214, 219, 223, 227, 232, 237, 241, 246, 251, 254, 259, 264, 269, 273, 278, 283, 287, 291, 296, 301, 305, 310, 314, 319, 323, 328, 333, 338, 342, 346, 351, 356, 360, 365, 370, 374, 378, 383, 388, 392, 397, 402, 406, 410, 415, 420, 425, 428, 432, 437, 442, 446, 451, 456, 460, 464, 468, 472, 476, 479, 483, 486, 488, 491, 493, 495, 497, 499, 500, 502, 503, 505, 506, 507, 507, 508, 508, 508, 508, 508, 508, 508, 508, 507, 507, 507, 507, 507, 506, 506, 506, 505, 505, 504, 504, 504, 503, 503, 502, 502, 501, 501, 501, 500, 500, 500, 500, 500, 500, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499];
+time_s = 0:samplingTime_s:samplingTime_s*(length(positionTo500_Kp500_counts)-1);
+data = {positionTo500_Kp500_counts, positionTo500_Kp1000_counts, positionTo500_Kp5000_counts};
+
+
+figure; hold on;
+for i = 1:3
+    color = Colors{i};
+    % Build plant model
+    Kp = Kps(i);
+    FWD = tf(Kp*V,65535) * Motor_tf_position;
+    Closed_tf = FWD / (1+FWD);
+    
+    % Predict values
+    expected = command*step(Closed_tf, time_s);
+
+    % Plot
+    plot(time_s, expected, '--', 'Color', color, 'LineWidth', 2);
+    plot(time_s, data{i}, 'Color', color, 'LineWidth', 2);
+end
+
+title("Motor Response to Positional Command (No Saturation)")
+xlabel("Time [s]");
+ylabel("Motor Position [counts]");
+legend('Kp = 500 - Expected', 'Kp = 500 - Measured', 'Kp = 1000 - Expected', 'Kp = 1000 - Measured', 'Kp = 5000 - Expected', 'Kp = 5000 - Measured', 'location', 'southeast');
+saveas(gcf, 'ControlCharacterization_noSaturation.png');
+
+%% Motor Positional Response to Positional Command - Simulink
+clc;clear;close all;
+Colors = {"#A2142F", "#7E2F8E", "#77AC30", "#0072BD"};
+
+command = 500; % counts
+V = 5; % volts
+Km = 182.7; % counts/(seconds*volts)
+tau = 50*10^(-3); % seconds
+Kps = [500, 1000, 5000];
+Kp = Kps(1);
+
+% Recorded data
+samplingTime_s = 1 / 250;
+sampleCount = 200;
+positionTo500_Kp500_counts = [ 0, 0, 1, 2, 4, 6, 8, 10, 12, 15, 18, 22, 25, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 73, 76, 80, 84, 89, 93, 98, 102, 107, 110, 115, 120, 124, 129, 133, 137, 142, 145, 150, 155, 159, 164, 168, 172, 177, 182, 186, 191, 195, 199, 204, 209, 213, 218, 223, 227, 231, 236, 241, 245, 250, 254, 258, 263, 268, 272, 277, 282, 286, 290, 295, 300, 304, 309, 314, 318, 322, 327, 332, 336, 341, 345, 350, 354, 359, 364, 368, 373, 377, 382, 385, 390, 394, 399, 401, 406, 410, 414, 418, 422, 426, 429, 432, 435, 439, 442, 445, 448, 450, 452, 455, 457, 459, 462, 463, 465, 467, 469, 470, 471, 473, 474, 475, 476, 478, 479, 480, 481, 482, 483, 483, 484, 485, 486, 487, 487, 488, 489, 489, 490, 490, 491, 492, 492, 493, 493, 494, 494, 495, 495, 496, 497, 497, 498, 498, 499, 499, 500, 500, 500, 501, 501, 501, 501, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502];
+positionTo500_Kp5000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 14, 18, 21, 24, 27, 31, 35, 39, 43, 47, 50, 54, 58, 63, 67, 71, 76, 79, 84, 88, 92, 97, 101, 105, 110, 114, 119, 123, 128, 133, 136, 141, 145, 150, 155, 159, 163, 168, 172, 177, 182, 186, 191, 195, 200, 204, 209, 214, 218, 223, 227, 231, 236, 241, 245, 250, 254, 259, 263, 268, 273, 276, 281, 285, 290, 294, 299, 304, 308, 312, 317, 322, 326, 331, 336, 340, 344, 349, 354, 358, 363, 368, 372, 376, 381, 386, 390, 395, 400, 404, 408, 413, 418, 423, 427, 431, 436, 440, 445, 450, 455, 459, 463, 468, 473, 477, 482, 487, 491, 495, 500, 504, 508, 510, 513, 514, 514, 513, 512, 510, 509, 507, 505, 503, 501, 499, 497, 496, 494, 494, 493, 493, 493, 494, 494, 495, 496, 498, 499, 500, 502, 503, 504, 505, 505, 506, 506, 506, 505, 504, 504, 503, 502, 501, 500, 499, 498, 498, 497, 496, 496, 496, 496, 496, 497, 497, 498, 498, 499, 499, 499, 500, 500, 500, 500, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501, 501];
+positionTo500_Kp1000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 15, 18, 21, 25, 28, 31, 35, 39, 43, 47, 50, 54, 58, 63, 67, 71, 76, 79, 84, 88, 93, 98, 102, 106, 110, 115, 120, 124, 129, 133, 137, 142, 146, 151, 156, 160, 164, 169, 174, 178, 183, 188, 192, 196, 201, 205, 209, 214, 219, 223, 227, 232, 237, 241, 246, 251, 254, 259, 264, 269, 273, 278, 283, 287, 291, 296, 301, 305, 310, 314, 319, 323, 328, 333, 338, 342, 346, 351, 356, 360, 365, 370, 374, 378, 383, 388, 392, 397, 402, 406, 410, 415, 420, 425, 428, 432, 437, 442, 446, 451, 456, 460, 464, 468, 472, 476, 479, 483, 486, 488, 491, 493, 495, 497, 499, 500, 502, 503, 505, 506, 507, 507, 508, 508, 508, 508, 508, 508, 508, 508, 507, 507, 507, 507, 507, 506, 506, 506, 505, 505, 504, 504, 504, 503, 503, 502, 502, 501, 501, 501, 500, 500, 500, 500, 500, 500, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499];
+time_s = 0:samplingTime_s:samplingTime_s*(length(positionTo500_Kp500_counts)-1);
+data = {positionTo500_Kp500_counts, positionTo500_Kp1000_counts, positionTo500_Kp5000_counts};
+expectedData = data;
+
+figure; hold on;
+for i = 1:3
+    color = Colors{i};
+
+    % Plot
+    plot(time_s, expectedData{i}, '--', 'Color', color, 'LineWidth', 2);
+    plot(time_s, data{i}, 'Color', color, 'LineWidth', 2);
+end
+
+title("Motor Response to Positional Command (Simulink)")
+xlabel("Time [s]");
+ylabel("Motor Position [counts]");
+legend('Kp = 500 - Expected', 'Kp = 500 - Measured', 'Kp = 1000 - Expected', 'Kp = 1000 - Measured', 'Kp = 5000 - Expected', 'Kp = 5000 - Measured', 'location', 'southeast');
+saveas(gcf, 'ControlCharacterization_Saturation.png');
+
+
+%% DATA COLLECTED FOR - Motor Positional Response to Positional Command
 Kp = 200;
 samplingTime_s = 1 / 1000;
 sampleCount = 200;
@@ -220,7 +436,6 @@ samplingTime_s = 1 / 250;
 sampleCount = 200;
 
 positionTo500_counts = [ 0, 0, 0, 0, 1, 2, 3, 4, 5, 7, 8, 10, 12, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 36, 37, 39, 42, 44, 46, 48, 50, 52, 54, 57, 59, 61, 63, 66, 67, 70, 72, 74, 76, 79, 81, 83, 85, 87, 90, 92, 94, 96, 98, 100, 103, 105, 107, 110, 111, 114, 116, 118, 120, 123, 125, 127, 129, 131, 134, 136, 138, 141, 142, 145, 147, 149, 151, 154, 156, 158, 160, 162, 165, 167, 169, 171, 173, 176, 178, 180, 182, 185, 186, 188, 191, 193, 194, 196, 198, 200, 201, 203, 205, 206, 208, 209, 210, 211, 213, 214, 215, 216, 217, 218, 219, 220, 221, 221, 222, 223, 224, 224, 225, 226, 226, 227, 228, 228, 229, 229, 230, 230, 231, 231, 232, 233, 233, 234, 234, 235, 235, 236, 236, 236, 236, 237, 238, 238, 239, 241, 243, 245, 248, 251, 253, 256, 259, 263, 266, 270, 273, 276, 280, 283, 287, 290, 293, 296, 299, 302, 305, 308, 311, 313, 316, 319, 322, 324, 327, 330, 333, 335, 337, 340, 342, 344, 346, 347, 349, 350, 351, 353, 354, 355, 356, 357, 357, 358, 359];
-time_ms = 0:5:5*(length(positionTo500_counts)-1);
 
 %
 % Fixed the weird non-saturation happening there
@@ -234,30 +449,7 @@ positionTo500_Kp5000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 14, 18, 21, 24, 2
 Kp = 1000;
 positionTo500_Kp1000_counts = [ 0, 0, 0, 0, 1, 3, 5, 6, 9, 12, 15, 18, 21, 25, 28, 31, 35, 39, 43, 47, 50, 54, 58, 63, 67, 71, 76, 79, 84, 88, 93, 98, 102, 106, 110, 115, 120, 124, 129, 133, 137, 142, 146, 151, 156, 160, 164, 169, 174, 178, 183, 188, 192, 196, 201, 205, 209, 214, 219, 223, 227, 232, 237, 241, 246, 251, 254, 259, 264, 269, 273, 278, 283, 287, 291, 296, 301, 305, 310, 314, 319, 323, 328, 333, 338, 342, 346, 351, 356, 360, 365, 370, 374, 378, 383, 388, 392, 397, 402, 406, 410, 415, 420, 425, 428, 432, 437, 442, 446, 451, 456, 460, 464, 468, 472, 476, 479, 483, 486, 488, 491, 493, 495, 497, 499, 500, 502, 503, 505, 506, 507, 507, 508, 508, 508, 508, 508, 508, 508, 508, 507, 507, 507, 507, 507, 506, 506, 506, 505, 505, 504, 504, 504, 503, 503, 502, 502, 501, 501, 501, 500, 500, 500, 500, 500, 500, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499, 499];
 
-
-time_ms = 0:5:5*(length(positionTo500_counts)-1);
-figure; hold on;
-plot(time_ms, positionTo500_Kp500_counts);
-plot(time_ms, positionTo500_Kp5000_counts);
-plot(time_ms, positionTo500_Kp1000_counts);
-title("Motor Response to Positional Command (1ms Sample Time)")
-xlabel("Time [ms]");
-ylabel("Motor Position [counts]");
-legend('Kp = 500', 'Kp = 5000', 'Kp = 1000', 'location', 'northwest');
-saveas(gcf, 'ControlCharacterization.png');
-
 positionTo250_Kp1000_counts = [ 0, 0, 0, 0, 2, 3, 5, 7, 10, 13, 16, 19, 23, 25, 29, 33, 37, 41, 45, 49, 52, 56, 61, 65, 69, 74, 77, 82, 85, 90, 94, 99, 103, 107, 112, 116, 121, 126, 130, 134, 139, 143, 148, 153, 157, 162, 166, 170, 175, 180, 185, 189, 193, 198, 202, 207, 211, 215, 219, 222, 226, 229, 233, 236, 239, 242, 244, 247, 249, 251, 253, 254, 256, 258, 259, 260, 260, 260, 261, 261, 261, 261, 261, 261, 261, 260, 260, 260, 259, 259, 258, 258, 257, 257, 257, 256, 256, 254, 254, 254, 254, 253, 253, 252, 252, 251, 251, 251, 250, 250, 250, 250, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249, 249];
-
-time_ms = 0:5:5*(length(positionTo500_counts)-1);
-figure; hold on;
-%plot(time_ms, positionTo250_Kp500_counts);
-%plot(time_ms, positionTo250_Kp5000_counts);
-plot(time_ms, positionTo250_Kp1000_counts);
-title("Motor Response to Positional Command (1ms Sample Time)")
-xlabel("Time [ms]");
-ylabel("Motor Position [counts]");
-legend('Kp = 500', 'Kp = 5000', 'Kp = 1000', 'location', 'northwest');
-saveas(gcf, 'ControlCharacterization_2.png');
 
 
 
