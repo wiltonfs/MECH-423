e81dcf0baba30137117392639db1bfafd4201e29
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2024-12-01 23:48:42 -0800

    PRJ - Printer tester in Windows Forms

---
+++ Project/PrinterTester_Forms/PrinterController.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Exercise6
+{
+    partial class PrinterController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.boardConnectedLabel = new System.Windows.Forms.Label();
+            this.visualTimer = new System.Windows.Forms.Timer(this.components);
+            this.postStringVisual = new System.Windows.Forms.Label();
+            this.preStringVisual = new System.Windows.Forms.Label();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.TxButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(13, 13);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.serialComboBox.TabIndex = 0;
+            this.serialComboBox.Text = "COM6";
+            this.serialComboBox.SelectedIndexChanged += new System.EventHandler(this.serialComboBox_SelectedIndexChanged);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(141, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(129, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh Serial Ports";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(276, 10);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(119, 23);
+            this.disconnectButton.TabIndex = 2;
+            this.disconnectButton.Text = "Disconnect Serial";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM6";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // boardConnectedLabel
+            // 
+            this.boardConnectedLabel.AutoSize = true;
+            this.boardConnectedLabel.Location = new System.Drawing.Point(14, 37);
+            this.boardConnectedLabel.Name = "boardConnectedLabel";
+            this.boardConnectedLabel.Size = new System.Drawing.Size(96, 13);
+            this.boardConnectedLabel.TabIndex = 18;
+            this.boardConnectedLabel.Text = "No board detected";
+            // 
+            // visualTimer
+            // 
+            this.visualTimer.Enabled = true;
+            this.visualTimer.Tick += new System.EventHandler(this.visualTimer_Tick);
+            // 
+            // postStringVisual
+            // 
+            this.postStringVisual.AutoSize = true;
+            this.postStringVisual.Location = new System.Drawing.Point(14, 380);
+            this.postStringVisual.Name = "postStringVisual";
+            this.postStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.postStringVisual.TabIndex = 33;
+            // 
+            // preStringVisual
+            // 
+            this.preStringVisual.AutoSize = true;
+            this.preStringVisual.Location = new System.Drawing.Point(14, 367);
+            this.preStringVisual.Name = "preStringVisual";
+            this.preStringVisual.Size = new System.Drawing.Size(0, 13);
+            this.preStringVisual.TabIndex = 34;
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(12, 72);
+            this.inputTextbox.Multiline = true;
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextbox.Size = new System.Drawing.Size(379, 144);
+            this.inputTextbox.TabIndex = 48;
+            // 
+            // TxButton
+            // 
+            this.TxButton.Location = new System.Drawing.Point(397, 134);
+            this.TxButton.Name = "TxButton";
+            this.TxButton.Size = new System.Drawing.Size(129, 23);
+            this.TxButton.TabIndex = 67;
+            this.TxButton.Text = "Transmit Text";
+            this.TxButton.UseVisualStyleBackColor = true;
+            this.TxButton.Click += new System.EventHandler(this.TransmitText_Click);
+            // 
+            // PrinterController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxButton);
+            this.Controls.Add(this.inputTextbox);
+            this.Controls.Add(this.preStringVisual);
+            this.Controls.Add(this.postStringVisual);
+            this.Controls.Add(this.boardConnectedLabel);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.serialComboBox);
+            this.Name = "PrinterController";
+            this.Text = "Gantry Commander";
+            this.Load += new System.EventHandler(this.DcMotorController_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialComboBox;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label boardConnectedLabel;
+        private System.Windows.Forms.Timer visualTimer;
+        private System.Windows.Forms.Label postStringVisual;
+        private System.Windows.Forms.Label preStringVisual;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.Button TxButton;
+    }
+}
+
---
+++ Project/PrinterTester_Forms/PrinterController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Exercise6
+{
+    public partial class PrinterController : Form
+    {
+        // --------------------------
+        // ----- Members & Data -----
+        // --------------------------
+
+        // ---------------------------------
+        // ----- Constructors and Load -----
+        // ---------------------------------
+        public PrinterController()
+        {
+            InitializeComponent();
+            //refreshSerialConnectionsComboBox();
+        }
+
+        private void DcMotorController_Load(object sender, EventArgs e)
+        {}
+
+        // ---------------------
+        // ----- UI Events -----
+        // ---------------------
+
+        // ----- Connecting to board -----
+        private void serialComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = serialComboBox.Text;
+                serialPort1.Open();
+            }
+        }
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshSerialConnectionsComboBox();
+        }
+        // ----- Printer buttons -----
+        private void TransmitText_Click(object sender, EventArgs e)
+        {
+            string tx = inputTextbox.Text;
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(tx);
+            }
+        }
+
+        // ----- Timers -----
+        private void visualTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshVisuals();
+        }
+
+        // -------------------------
+        // ----- Serial Events -----
+        // -------------------------
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+        // ------------------------
+        // ----- UI Functions -----
+        // ------------------------
+
+        private void RefreshVisuals()
+        {
+            if (!serialPort1.IsOpen)
+            { boardConnectedLabel.Text = "No board detected"; }
+            else
+            { boardConnectedLabel.Text = "Board connected"; }
+        }
+        void refreshSerialConnectionsComboBox()
+        {
+            // Refresh the combo box with the available COM ports
+            serialComboBox.Items.Clear();
+            serialComboBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (serialComboBox.Items.Count == 0)
+                serialComboBox.Text = "No COM ports!";
+            else
+                serialComboBox.SelectedIndex = 0;
+        }
+    }
+}
